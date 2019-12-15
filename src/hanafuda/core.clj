3f58f255b8653c9604e11eade9c3fe30fee8fe39
Michael Daines <953037+defndaines@users.noreply.github.com>
2019-12-15 15:31:12 -0800

    Old changes from last time I was here

--- src/hanafuda/core.clj
+++ src/hanafuda/core.clj
@@ -1,6 +1,13 @@
-(ns hanafuda.core)
+(ns hanafuda.core
+  "Hanafuda core functionaltiy."
+  (:require [hanafuda.cards :as cards]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn deal
+  "Deal out cards for a round of play."
+  []
+  )
